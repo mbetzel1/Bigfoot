c05288210c2671690986df425984a5b140713bcc
mbetz <mbetzel1@gmail.com>
2023-12-17 18:25:04 -0500

    initial

---
+++ combined/js/circle_chart.js
@@ -0,0 +1,210 @@
+//Either end of Viridis palette.
+var circle_palette = ['#440154', '#FDE725']
+
+function circularHeatChart() {
+    //set some constants
+    var margin = {top: 10, right: 5, bottom: 5, left: 25},
+    innerRadius = 7,
+    numSegments = 12,
+    segmentHeight = 7,
+    domain = null,
+    accessor = function(d) {return d;},
+    radialLabels = segmentLabels = [];
+
+    function chart(selection) {
+        selection.each(function(data) {
+            var svg = d3.select(this);
+            var offset =  5 + innerRadius + Math.ceil(data.length / numSegments) * (segmentHeight);
+            g = svg.append("g")
+                .classed("circular-heat", true)
+                .attr("transform", "translate(" + parseInt(margin.left + offset) + "," + parseInt(margin.top + offset) + ")");
+
+            var autoDomain = false;
+            if (domain === null) {
+                domain = d3.extent(data, accessor);
+                autoDomain = true;
+            }
+            var color = d3.scale.linear().domain(domain).range(range);
+            if(autoDomain)
+                domain = null;
+
+            g.selectAll("path").data(data)
+                .enter().append("path")
+                .attr("d", d3.svg.arc().innerRadius(ir).outerRadius(or).startAngle(sa).endAngle(ea))
+                .attr("fill", function(d) {return color(accessor(d));});
+
+            // Unique id so that the text path defs are unique - is there a better way to do this?
+            var id = d3.selectAll(".circular-heat")[0].length;
+
+            //Radial labels
+            // var lsa = 0.01; //Label start angle
+            // var labels = svg.append("g")
+            //     .classed("labels", true)
+            //     .classed("radial", true)
+            //     .attr("transform", "translate(" + parseInt(margin.left + offset) + "," + parseInt(margin.top + offset) + ")");
+
+            // //add the label paths
+            // labels.selectAll("def")
+            //     .data(radialLabels).enter()
+            //     .append("def")
+            //     .append("path")
+            //     .attr("id", function(d, i) {return "radial-label-path-"+id+"-"+i;})
+            //     .attr("d", function(d, i) {
+            //         var r = innerRadius + ((i + 0.2) * segmentHeight);
+            //         return "m" + r * Math.sin(lsa) + " -" + r * Math.cos(lsa) + 
+            //                 " a" + r + " " + r + " 0 1 1 -1 0";
+            //     })
+            // //add the label text
+            // labels.selectAll("text")
+            //     .data(radialLabels).enter()
+            //     .append("text")
+            //     .append("textPath")
+            //     .attr("xlink:href", function(d, i) {return "#radial-label-path-"+id+"-"+i;})
+            //     .text(function(d) {return d;});
+
+            //Segment labels
+            var segmentLabelOffset = 2;
+            var r = innerRadius + Math.ceil(data.length / numSegments) * segmentHeight + segmentLabelOffset;
+            labels = svg.append("g")
+                .classed("labels", true)
+                .classed("segment", true)
+                .attr("transform", "translate(" + parseInt(margin.left + offset) + "," + parseInt(margin.top + offset) + ")");
+
+            labels.append("def")
+                .append("path")
+                .attr("id", "segment-label-path-"+id)
+                .attr("d", "m0 -" + r + " a" + r + " " + r + " 0 1 1 -1 0");
+
+            labels.selectAll("text")
+                .data(segmentLabels).enter()
+                .append("text")
+                .append("textPath")
+                .attr("xlink:href", "#segment-label-path-"+id)
+                .attr("startOffset", function(d, i) {return i * 100 / numSegments + "%";})
+                .text(function(d) {return d;});
+        });
+
+    }
+
+    /* Arc functions */
+    ir = function(d, i) {
+        return innerRadius + Math.floor(i/numSegments) * segmentHeight;
+    }
+    or = function(d, i) {
+        return innerRadius + segmentHeight + Math.floor(i/numSegments) * segmentHeight;
+    }
+    sa = function(d, i) {
+        return (i * 2 * Math.PI) / numSegments;
+    }
+    ea = function(d, i) {
+        return ((i + 1) * 2 * Math.PI) / numSegments;
+    }
+
+    // Configuration getters/setters 
+    chart.margin = function(_) {
+        if (!arguments.length) return margin;
+        margin = _;
+        return chart;
+    };
+    chart.innerRadius = function(_) {
+        if (!arguments.length) return innerRadius;
+        innerRadius = _;
+        return chart;
+    };
+    chart.numSegments = function(_) {
+        if (!arguments.length) return numSegments;
+        numSegments = _;
+        return chart;
+    };
+    chart.segmentHeight = function(_) {
+        if (!arguments.length) return segmentHeight;
+        segmentHeight = _;
+        return chart;
+    };
+    chart.domain = function(_) {
+        if (!arguments.length) return domain;
+        domain = _;
+        return chart;
+    };
+    chart.range = function(_) {
+        if (!arguments.length) return range;
+        range = _;
+        return chart;
+    };
+    chart.radialLabels = function(_) {
+        if (!arguments.length) return radialLabels;
+        if (_ == null) _ = [];
+        radialLabels = _;
+        return chart;
+    };
+    chart.segmentLabels = function(_) {
+        if (!arguments.length) return segmentLabels;
+        if (_ == null) _ = [];
+        segmentLabels = _;
+        return chart;
+    };
+    chart.accessor = function(_) {
+        if (!arguments.length) return accessor;
+        accessor = _;
+        return chart;
+    };
+    return chart;
+}
+
+
+//PROCESSING
+
+data = [];
+d3.csv("data/bf_months_final.csv", function(error, data) {
+	  
+	//go through each line, can modify contents as need be
+	data.forEach(function(d){
+			
+		//sets the specfic values in the data array
+		d.year = d.Year
+		d.month = d.Month
+		d.value = +d.Value
+		//return {"month": m, "value":v} ;
+			 
+	});
+
+// var radLabels = ['', '', '', '', '', 1985, '', '', '', '', 1990,
+//  '', '', '', '', 1995, '', '', '', '', 2000,
+//   '', '', '', '', 2005, '', '', '', '', 2010,
+//    '', '', '', '', 2015, '', '', '']
+
+var segLabels = ["January", "February", "March", "April", "May", "June",
+ "July", "August", "September", "October", "November", "December"]
+ 
+var chart = circularHeatChart()
+	.innerRadius(7)
+    .segmentHeight(7)
+    .numSegments(12) // define the overall shape
+    .range(circle_palette)
+    //.radialLabels(radLabels)
+    .segmentLabels(segLabels);
+	
+	chart.accessor(function(d) {return d.value;})
+	
+//add the chart to the parent
+d3.select('#circle-div')
+    .selectAll('svg')
+    .data([data])
+    .enter()
+    .insert('svg', ".chart-title")
+    .call(chart);
+
+/* events */
+d3.selectAll("#circle-div path").on('mouseover', function() {
+	var d = d3.select(this).data()[0];
+    d3.select("#info").text(d.month + '/' + d.year + ' had ' + d.value + ' bigfoot sightings');
+});
+d3.selectAll("#circle-div svg").on('mouseout', function() {
+    d3.select("#info").text('');	
+});
+// d3.selectAll("#circle-div svg").on('click', function() {
+//     alert('click!');	
+// });
+
+});
+
---
+++ combined/js/google_charts.js
@@ -0,0 +1,172 @@
+//colors
+var palette = ["#440154","#440256","#450457","#450559","#46075a","#46085c","#460a5d","#460b5e","#470d60","#470e61","#471063","#471164","#471365",
+"#481467","#481668","#481769","#48186a","#481a6c","#481b6d","#481c6e","#481d6f","#481f70","#482071","#482173","#482374","#482475","#482576",
+"#482677","#482878","#482979","#472a7a","#472c7a","#472d7b","#472e7c","#472f7d","#46307e","#46327e","#46337f","#463480","#453581","#453781",
+"#453882","#443983","#443a83","#443b84","#433d84","#433e85","#423f85","#424086","#424186","#414287","#414487","#404588","#404688","#3f4788",
+"#3f4889","#3e4989","#3e4a89","#3e4c8a","#3d4d8a","#3d4e8a","#3c4f8a","#3c508b","#3b518b","#3b528b","#3a538b","#3a548c","#39558c","#39568c",
+"#38588c","#38598c","#375a8c","#375b8d","#365c8d","#365d8d","#355e8d","#355f8d","#34608d","#34618d","#33628d","#33638d","#32648e","#32658e",
+"#31668e","#31678e","#31688e","#30698e","#306a8e","#2f6b8e","#2f6c8e","#2e6d8e","#2e6e8e","#2e6f8e","#2d708e","#2d718e","#2c718e","#2c728e",
+"#2c738e","#2b748e","#2b758e","#2a768e","#2a778e","#2a788e","#29798e","#297a8e","#297b8e","#287c8e","#287d8e","#277e8e","#277f8e","#27808e",
+"#26818e","#26828e","#26828e","#25838e","#25848e","#25858e","#24868e","#24878e","#23888e","#23898e","#238a8d","#228b8d","#228c8d","#228d8d",
+"#218e8d","#218f8d","#21908d","#21918c","#20928c","#20928c","#20938c","#1f948c","#1f958b","#1f968b","#1f978b","#1f988b","#1f998a","#1f9a8a",
+"#1e9b8a","#1e9c89","#1e9d89","#1f9e89","#1f9f88","#1fa088","#1fa188","#1fa187","#1fa287","#20a386","#20a486","#21a585","#21a685","#22a785",
+"#22a884","#23a983","#24aa83","#25ab82","#25ac82","#26ad81","#27ad81","#28ae80","#29af7f","#2ab07f","#2cb17e","#2db27d","#2eb37c","#2fb47c",
+"#31b57b","#32b67a","#34b679","#35b779","#37b878","#38b977","#3aba76","#3bbb75","#3dbc74","#3fbc73","#40bd72","#42be71","#44bf70","#46c06f",
+"#48c16e","#4ac16d","#4cc26c","#4ec36b","#50c46a","#52c569","#54c568","#56c667","#58c765","#5ac864","#5cc863","#5ec962","#60ca60","#63cb5f",
+"#65cb5e","#67cc5c","#69cd5b","#6ccd5a","#6ece58","#70cf57","#73d056","#75d054","#77d153","#7ad151","#7cd250","#7fd34e","#81d34d","#84d44b",
+"#86d549","#89d548","#8bd646","#8ed645","#90d743","#93d741","#95d840","#98d83e","#9bd93c","#9dd93b","#a0da39","#a2da37","#a5db36","#a8db34",
+"#aadc32","#addc30","#b0dd2f","#b2dd2d","#b5de2b","#b8de29","#bade28","#bddf26","#c0df25","#c2df23","#c5e021","#c8e020","#cae11f","#cde11d",
+"#d0e11c","#d2e21b","#d5e21a","#d8e219","#dae319","#dde318","#dfe318","#e2e418","#e5e419","#e7e419","#eae51a","#ece51b","#efe51c","#f1e51d",
+"#f4e61e","#f6e620","#f8e621","#fbe723","#fde725"]
+//fontsize
+
+var fs = 24
+// MAP
+
+google.charts.load('current', {
+    'packages': ['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  });
+
+
+
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap() {
+    var options = {
+        region: 'US',
+        displayMode: 'regions',
+        resolution: 'provinces',
+        colorAxis: {colors: palette},
+        backgroundColor: '#FFFFFF',
+        datalessRegionColor: '#000000',
+        defaultColor: '#000000',
+        width: "100%",
+        height: "100%",
+        legend: {textStyle: {fontSize: fs}},
+        tooltip: {textStyle: {fontSize: fs}}
+    };
+    var data = google.visualization.arrayToDataTable([
+        ['State', 'Select'],
+        ['US-AL', 91],
+        ['US-AK', 20],
+        ['US-AR', 84],
+        ['US-AZ', 95],
+        ['US-CA', 402],
+        ['US-CT', 15],
+        ['US-CO', 121],
+        ['US-DE', 5],
+        ['US-FL', 292],
+        ['US-GA', 120],
+        ['US-HI', 0],
+        ['US-IN', 75],
+        ['US-IL', 232],
+        ['US-IA', 56],
+        ['US-KS', 42],
+        ['US-KY', 112],
+        ['US-MI', 208],
+        ['US-MO', 141],
+        ['US-MS', 21],
+        ['US-MT', 45],
+        ['US-ME', 14],
+        ['US-MA', 27],
+        ['US-MN', 69],
+        ['US-MT', 45],
+        ['US-NE', 15],
+        ['US-NJ', 63],
+        ['US-NM', 40],
+        ['US-NY', 101],
+        ['US-NC', 79],
+        ['US-ND', 4],
+        ['US-NH', 13],
+        ['US-NV', 7],
+        ['US-OH', 276],
+        ['US-OK', 85],
+        ['US-OR', 241],
+        ['US-PA', 111],
+        ['US-RI', 5],
+        ['US-SC', 38],
+        ['US-SD', 11],
+        ['US-TX', 215],
+        ['US-TN', 91],
+        ['US-UT', 57],
+        ['US-VA', 72],
+        ['US-VT', 9],
+        ['US-WA', 563],
+        ['US-WV', 100],
+        ['US-WY', 27],
+        ['US-WI', 27],
+        ['US-ID', 81],
+        ['US-LA', 40],
+        ['US-MD', 34],
+    ]);
+
+    var chart = new google.visualization.GeoChart(document.getElementById('map-div'));
+
+    chart.draw(data, options);
+  }
+
+//LINE CHART
+
+google.load('visualization', '1', { packages: ['corechart', 'controls'] });
+
+function drawVisualization() {
+    $.get("data/bf_years.csv", function(csvString) {
+       // transform the CSV string into a 2-dimensional array
+       var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
+       var years = arrayData.map(function(value,index) { return new Date(value[0], 1, 1); })
+       
+       // this should probably be somewhere else but I don't have time
+       function combineColumns(array1, array2) {
+
+          // Combine columns from array1 and array2 into a new array
+          var combinedArray = array1.map(function (value, index) {
+       
+             return [array2[index], value[1]];
+          });
+
+          return combinedArray;
+       }
+       combinedArray = combineColumns(arrayData, years)
+
+       // this new DataTable object holds all the data
+       var data = new google.visualization.arrayToDataTable(combinedArray);
+
+
+       var line_chart = new google.visualization.ChartWrapper({
+          chartType: 'LineChart',
+          containerId: 'line-div',
+          dataTable: data,
+          options:{
+             width: "100%", 
+             height: "100%",
+             colors: palette,
+             tooltip: {textStyle: {fontSize: fs}},
+             vAxis: {
+               title: 'Encounters',
+               titleTextStyle: {
+                  fontSize: fs
+               }
+            },
+             hAxis: {
+               title: 'Year',
+               titleTextStyle: {
+                  fontSize: fs,
+               }
+            },
+             
+            legend: {
+               //position: 'top',
+               position: 'none',
+               textStyle: {
+                  fontSize: fs,
+               },
+            },
+          }
+       });
+       line_chart.draw();
+    });
+ }
+ google.setOnLoadCallback(drawVisualization)
